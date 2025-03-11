bc24458cfe39fae78b43c9e83838ef950790bee7
Prithviraj V <prithvirajv@outlook.com>
2025-03-11 18:26:24 +0530

    AS part of this commit we are adding - Config Server - Service Registery - API gateway Skeleton

---
+++ config-server/src/test/java/com/bookameet/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookameet.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
